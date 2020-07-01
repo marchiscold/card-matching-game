7a81dbceaa35c634283b501120090efc15baf87f
Pavel Martynov <martynovdev@gmail.com>
2020-07-01 11:54:50 +0300

    minor

--- src/App.js
+++ src/App.js
@@ -30,24 +30,6 @@ class App extends React.Component {
       </div>
     );
   }
-/*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-*/
 }
 
 export default App;
