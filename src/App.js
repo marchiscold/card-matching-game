aa2433d08a7973ccec5e05f8243e7c5dbbadf0e4
Pavel Martynov <martynovdev@gmail.com>
2020-07-12 15:02:20 +0300

    Add limit on open cards

--- src/App.js
+++ src/App.js
@@ -9,22 +9,25 @@ class App extends React.Component {
     const CARDS_AMOUNT = 4;
     const cards = this.shuffle(this.generateCards(CARDS_AMOUNT));
     this.state = {
-      cards: cards
+      cards: cards,
+      openCardsIds: [],
+      matched: 0,
     };
     this.handleClick = this.handleClick.bind(this);
   }
   
-  generateCards (num) {
+  generateCards (cardAmount) {
     let cards = [];
-    let colors = ['rebeccapurple', 'goldenrod', 'palegreen', 'lightcoral'];
-    for (let i = 0; i < num; i++) {
+    let colors = ['', 'rebeccapurple', 'goldenrod', 'palegreen', 'lightcoral'];
+    for (let i = 1; i < cardAmount + 1; i++) {
       let card = {
         isOpen: false,
+        isMatched: false,
         color: colors[i],
-        id: colors[i],
+        id: i,
       };
       cards.push(card);
-      cards.push({...card});
+      cards.push({...card, ...{id: -i}});
     }
     
     return cards;
@@ -36,9 +39,28 @@ class App extends React.Component {
   
   handleClick(cardNum) {
     let newCards = [...this.state.cards];
-    newCards[cardNum].isOpen = !newCards[cardNum].isOpen;
+    let openCardsIds = this.state.openCardsIds;
+
+    if (!newCards[cardNum].isOpen) {
+      newCards[cardNum].isOpen = true;
+      openCardsIds.push(newCards[cardNum].id);
+    }
+
+    if (openCardsIds.length > 2) {
+      let firstTwoIds = openCardsIds.slice(0, 2);
+      newCards = newCards.map(card => {
+        if (firstTwoIds.includes(card.id)) {
+          card.isOpen = false;
+          return card;
+        }
+        return card;
+      });
+      openCardsIds = openCardsIds.slice(2);
+    }
+
     this.setState({
-      cards: newCards
+      cards: newCards,
+      openCardsIds: openCardsIds,
     });
   }
 
