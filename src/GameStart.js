fcd1f0a97bc30c0fb7cacbaaabdef0574d029fe5
Pavel Martynov <martynovdev@gmail.com>
2020-07-18 10:12:15 +0300

    Finish start screen ui styling

--- src/GameStart.js
+++ src/GameStart.js
@@ -13,15 +13,17 @@ function GameStart({ onGameStart, fadeout, fadein }) {
     <div className={classname}>
       <h1 className='start-screen__name'>Match it</h1>
       <div className='start-screen__mode mode'>
-        <div className='mode__item' style={{backgroundImage: `url(${bugLogo})`}}>
-          <div className='mode__overlay'>bugs</div>
+        <div className='mode__item active' style={{backgroundImage: `url(${bugLogo})`}}>
+          <div className='mode__overlay'></div>
+          <div className='mode__description'>bugs</div>
         </div>
         <div className='mode__item' style={{backgroundImage: `url(${fishLogo})`}}>
-          <div className='mode__overlay'>aquatic</div>
+          <div className='mode__overlay'></div>
+          <div className='mode__description'>aquatic</div>
         </div>
       </div>
       <div className='start-screen__grid grid-options'>
-        <div className='grid-options__item'>2x4</div>
+        <div className='grid-options__item active'>2x4</div>
         <div className='grid-options__item'>4x4</div>
       </div>
       <button className='start-screen__button' onClick={onGameStart}>start</button>
