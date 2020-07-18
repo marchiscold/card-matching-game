dcc52894a1c5147db1fe22ceb98e36759c56e19f
Pavel Martynov <martynovdev@gmail.com>
2020-07-18 08:30:30 +0300

    Style start screen

--- src/GameStart.js
+++ src/GameStart.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import classnames from 'classnames'
+import React from 'react';
+import classnames from 'classnames';
+import './GameStart.css';
 
 function GameStart({ onGameStart, fadeout, fadein }) {
-  const classname = classnames("start-screen", {
-    "start-screen--fadeout": fadeout,
-    "start-screen--fadein": fadein,
+  const classname = classnames('start-screen', {
+    'start-screen--fadeout': fadeout,
+    'start-screen--fadein': fadein,
   });
   return (
     <div className={classname}>
+      <h1 className='start-screen__name'>Match it</h1>
+      <div className='start-screen__mode mode'>
+        <div className='mode__item'>bugs</div>
+        <div className='mode__item'>dinosaurs</div>
+      </div>
+      <div className='start-screen__grid grid-options'>
+        <div className='grid-options__item'>2x4</div>
+        <div className='grid-options__item'>4x4</div>
+      </div>
       <button className='start-screen__button' onClick={onGameStart}>start</button>
     </div>
   )
