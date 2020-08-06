63ba694013a3d6110a7d6ab8b46612151c960e21
Pavel Martynov <martynovdev@gmail.com>
2020-08-06 14:42:28 +0300

    Finish card styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,21 +4,21 @@ import styled from './Card.module.css';
 
 function Card({ id, card, onClick }) {
   let cardClass = classnames({
-    card: true,
-    'card--matched': card.isMatched,
-    'card--open': card.isOpen
+    [styled['card']]: true,
+    [styled['card--matched']]: card.isMatched,
+    [styled['card--open']]: card.isOpen
   });
 
-  let style = {};
+  let frontStyle = {};
   if (card.isMatched || card.isOpen) {
-    style.backgroundColor = card.color;
-    style.backgroundImage = `url(${card.img})`;
+    frontStyle.backgroundColor = card.color;
+    frontStyle.backgroundImage = `url(${card.img})`;
   }
 
   return (
     <div className={cardClass} onClick={() => onClick(id)}>
-      <div className={styled['card--front']}></div>
-      <div className={styled['card--back']} card={card} style={style}></div>
+      <div className={styled['card__inner--front'] + ' ' + styled['card__inner']}></div>
+      <div className={styled['card__inner--back'] + ' ' + styled['card__inner']} card={card} style={frontStyle}></div>
     </div>
   );
 }
