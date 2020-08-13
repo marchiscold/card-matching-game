0230c0a4462ee23db774dacc38bc3e50f4aa9595
Pavel Martynov <martynovdev@gmail.com>
2020-08-13 18:31:17 +0300

    Add card panel component

--- src/components/GameBoard/CardPanel/CardPanel.js
+++ src/components/GameBoard/CardPanel/CardPanel.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card/Card';
+import styles from '../GameBoard.module.css';
 
-function CardPanel() {
+function CardPanel({ cards, grid, onCardClick }) {
+  const cardList = cards.map((card, index) => {
+    return <Card key={index} 
+                 id={index} 
+                 card={card}
+                 onClick={onCardClick}/>;
+  });
+  
+  const rows = grid[0];
+  const cardsPerRow = cards.length/rows;
+  const cardRows = [];
+  for (let row = 0; row < rows; row++) {
+    const cardRow = [];
+    for (let i = 0; i < cardsPerRow; i++) {
+      cardRow.push(cardList[cardsPerRow*row + i]);
+    }
+    cardRows.push(<div key={row} className={styles['board__row']}>{cardRow}</div>);
+  }
+
   return (
-    <div>
-      
-    </div>
+    <div className={styles['board']}>{cardRows}</div>
   )
 }
 
--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,36 +1,19 @@
 import React from 'react';
-import Card from './CardPanel/Card/Card';
 import InfoPanel from './InfoPanel/InfoPanel';
+import CardPanel from './CardPanel/CardPanel';
 import classnames from 'classnames';
 import styles from './GameBoard.module.css';
 
 function GameBoard({ tries, cards, grid, timer, onCardClick, fadeout }) {
-  const rows = grid[0];
-  const cardList = cards.map((card, index) => {
-    return <Card key={index} 
-                 id={index} 
-                 card={card}
-                 onClick={onCardClick}/>;
-  });
-
-  const cardsPerRow = cards.length/rows;
-  const cardRows = [];
-  for (let row = 0; row < rows; row++) {
-    const cardRow = [];
-    for (let i = 0; i < cardsPerRow; i++) {
-      cardRow.push(cardList[cardsPerRow*row + i]);
-    }
-    cardRows.push(<div key={row} className={styles['board__row']}>{cardRow}</div>);
-  }
-
   const gameScreenClass = classnames({
     [styles['game-screen']]: true,
     [styles['game-screen--fadeout']]: fadeout,
   });
+
   return (
     <div className={gameScreenClass}>
       <InfoPanel cards={cards} tries={tries} timer={timer}/>
-      <div className={styles['board']}>{cardRows}</div>
+      <CardPanel cards={cards} grid={grid} onCardClick={onCardClick}/>
     </div>
   );
 }
